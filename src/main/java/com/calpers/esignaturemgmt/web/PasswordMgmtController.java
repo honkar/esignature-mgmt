f0b9ebe9d2455679325395d1f3bebe337cbf091f
harshitha <harshithaonkar@gmail.com>
2020-06-29 12:51:06 -0700

    ESM-33: Change Password Mgmt
    added change_pwd html page
    Password mgmt controller

---
+++ src/main/java/com/calpers/esignaturemgmt/web/PasswordMgmtController.java
@@ -0,0 +1,75 @@
+package com.calpers.esignaturemgmt.web;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.calpers.esignaturemgmt.model.User;
+import com.calpers.esignaturemgmt.repository.UserRepository;
+import com.calpers.esignaturemgmt.service.NotificationService;
+import com.calpers.esignaturemgmt.service.UserService;
+import com.calpers.esignaturemgmt.web.dto.EmailAjax;
+import com.calpers.esignaturemgmt.web.dto.PwdAjax;
+import com.calpers.esignaturemgmt.web.dto.UserSessionDto;
+
+@RestController
+public class PasswordMgmtController {
+	
+	private Logger logger = LoggerFactory.getLogger(PasswordMgmtController.class);
+
+	@Autowired
+	HttpSession session;
+
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	private NotificationService notifyService;
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@RequestMapping(value = "/changepwd", method = RequestMethod.POST)
+	public ResponseEntity<Object> changePassword(@RequestBody PwdAjax pwdObj) {
+		UserSessionDto userObj = (UserSessionDto) session.getAttribute("userDetails");
+		User user = userService.findByEmail(userObj.getEmail());
+		if (user != null && passwordEncoder.matches(pwdObj.getCurrentPwd(), user.getPassword())) {
+			user.setPassword(passwordEncoder.encode(pwdObj.getNewPwd()));
+			User updatedUser = userRepository.save(user);
+			if(updatedUser!=null) {
+				return new ResponseEntity<>("Success",HttpStatus.OK);
+			}
+		}
+		return new ResponseEntity<>("Error", HttpStatus.BAD_REQUEST);
+	}
+	
+	@RequestMapping(value = "/resetpwd", method = RequestMethod.POST)
+	public ResponseEntity<Object> resetPassword(@RequestBody EmailAjax emailObj) {
+		User existing = userService.findByEmail(emailObj.getEmail());
+		if(existing!=null) {
+			try {
+				notifyService.resetPassword(existing);
+			} catch (Exception e) {
+
+				logger.info("Error while sending email "+ e.getMessage());
+			}
+			return new ResponseEntity<>("Success", HttpStatus.OK);
+		}
+	
+		return new ResponseEntity<>("Error", HttpStatus.BAD_REQUEST);
+	}
+	
+
+}
---
+++ src/main/java/com/calpers/esignaturemgmt/web/dto/PwdAjax.java
@@ -0,0 +1,23 @@
+package com.calpers.esignaturemgmt.web.dto;
+
+public class PwdAjax {
+	
+	private String currentPwd;
+	private String newPwd;
+	
+	public String getCurrentPwd() {
+		return currentPwd;
+	}
+	public void setCurrentPwd(String currentPwd) {
+		this.currentPwd = currentPwd;
+	}
+	public String getNewPwd() {
+		return newPwd;
+	}
+	public void setNewPwd(String newPwd) {
+		this.newPwd = newPwd;
+	}
+	
+	
+
+}
